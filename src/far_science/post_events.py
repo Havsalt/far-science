8dd75204353ef8a61e139cbd5339df728c81f4c8
Havsalt <77575424+Havsalt@users.noreply.github.com>
2026-02-14 21:39:53 +0100

    Edited comment

--- src/far_science/post_events.py
+++ src/far_science/post_events.py
@@ -1,6 +1,9 @@
 from .context import Context
 
 
-# This is just a type trigger, since `lambda` does not allow assignments
+# These functions are just a function that mutates the context,
+# since `lambda` does not allow assignments
+
+
 def complete_initial_reports_for_ai(ctx: Context) -> None:
     ctx.state.completed_initial_reports_for_ai = True
